3c2caf1d76d5c80a8d233c39def1ccc235cd0e61
Xiagir <513535b@gmail.com>
2018-11-23 23:39:05 -0500

    Initial Xiagir

---
+++ Scripts/ElevatorController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorController : MonoBehaviour {
+
+    int blocksToTp = 9;
+ 
+  
+    public PlayeerMovement player;
+    public EntityController entity;
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            player.elevatorChecked = true;
+        }
+        else if (collision.gameObject.tag == "Entity") {  
+            if (EntityController.useElevator())
+                Elevate(collision.gameObject);
+        }
+    }
+
+   void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            player.elevatorChecked = false;
+        }
+    }
+
+    public void Elevate(GameObject character)
+    {
+        character.transform.position = new Vector2(character.transform.position.x, character.transform.position.y + blocksToTp);
+    }
+
+
+}
---
+++ Scripts/EntityController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityController : MonoBehaviour {
+
+    int entityExists = 0;
+    public float entityQuantity = 5;
+
+    public GameObject entity;
+
+
+    private float[,] points = new float[,] { 
+        {-5,-3},
+        {20,-3},
+        {40,-3}
+};
+
+    private float[] pointsToSpawn = new float[] {-5,20,40};
+
+
+    static public bool useElevator()
+    {
+        int rng = Random.Range(0, 2);
+        if (rng == 1)
+            return true;
+        else 
+            return false;
+    }
+	
+	void Update () {
+
+        if (entityExists < entityQuantity)
+        {
+            Vector3 Pos = new Vector3(pointsToSpawn[Random.Range(0,2)] , -3 , 0);
+            Instantiate(entity , Pos , Camera.main.transform.rotation);
+            entityExists++;
+        }
+
+       
+		
+	}
+}
